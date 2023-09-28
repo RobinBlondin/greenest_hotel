581545a9a8506fc0f9725c8c2308d69c69a37e3c
Robin Blondin <robin.blondin@protonmail.com>
2023-09-28 22:13:41 +0200

    Set up palm tree sub class

---
+++ src/PalmTree.java
@@ -0,0 +1,10 @@
+public class PalmTree extends Vegetation {
+    public PalmTree(String name, double height) {
+        super(name, height);
+    }
+
+    @Override
+    public void waterPlant() {
+
+    }
+}
