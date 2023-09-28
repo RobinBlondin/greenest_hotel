b730440e91ca324862f0e57c7058dc9f4b451257
Robin Blondin <robin.blondin@protonmail.com>
2023-09-28 22:01:53 +0200

    Set up an enum using the different food sources for the plants.

---
+++ src/FoodSource.java
@@ -0,0 +1,11 @@
+public enum FoodSource {
+    TAP_WATER("tap water"),
+    PROTEIN_SHAKE("protein shake"),
+    MINERAL_WATER("mineral water");
+
+    public final String foodSource;
+
+    FoodSource(String foodSource) {
+        this.foodSource = foodSource;
+    }
+}
