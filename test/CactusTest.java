7eb2fbf0a5925c9705dae043970155af6cbbf9d2
Robin Blondin <robin.blondin@protonmail.com>
2023-09-28 23:28:19 +0200

    Added test code for waterPlant method

--- test/CactusTest.java
+++ test/CactusTest.java
@@ -4,7 +4,9 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class CactusTest {
 
+    Cactus cactus = new Cactus("Igge", 0.2);
     @Test
-    void waterPlant() {
+    public final void waterPlant() {
+        assertEquals(0.02, cactus.waterPlant());
     }
 }
