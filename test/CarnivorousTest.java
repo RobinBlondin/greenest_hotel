f1c8f7060ed9819ad1274a915abf1e519d5638ba
Robin Blondin <robin.blondin@protonmail.com>
2023-09-28 23:45:43 +0200

    Added tests for waterPlant method in Carnivorous class

--- test/CarnivorousTest.java
+++ test/CarnivorousTest.java
@@ -3,8 +3,9 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class CarnivorousTest {
-
+    Carnivorous carnivorous = new Carnivorous("Meatloaf", 0.7);
     @Test
-    void waterPlant() {
+    public final void waterPlant() {
+        assertEquals(0.24, carnivorous.waterPlant());
     }
 }
