34fab2dd27f768f87ec3be0b8531cfd5875101da
Robin Blondin <robin.blondin@protonmail.com>
2023-09-28 22:12:54 +0200

    Set up cactus sub class

---
+++ src/Cactus.java
@@ -0,0 +1,11 @@
+public class Cactus extends Vegetation{
+
+    public Cactus(String name, double height) {
+        super(name, height);
+    }
+
+    @Override
+    public void waterPlant() {
+
+    }
+}
